d77f0634d9824f97b2420117f6ff85857e18bc0d
ciser <honseung2@gmail.com>
2023-11-01 15:16:08 +0900

    계산기+자잘한 수정

--- functions.js
+++ functions.js
@@ -23,17 +23,75 @@ function nightDayHandler(self) {
         Body.setBackgroundColor('black');
         Body.setColor('white');
         self.value = 'day';
-
         Links.setColor('lightgreen');
     } else {
         Body.setBackgroundColor('white');
         Body.setColor('black');
         self.value = 'night';
-
         Links.setColor('orange');
     }
 }
-// 주사위
-function randice(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+// // 주사위
+// function randice(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+//계산기
+var input1 = 0
+var input2 = 0
+var operator = ''
+
+function clrScreen() {
+    document.forms.result.value='';
+}
+function clickNumber(n) {
+    document.forms.result.value+=n;
+}
+
+function clickPlus() {
+    input1 = parseFloat(document.forms.result.value);
+    // save value to variable and operator
+    operator = '+';
+    document.forms.result.value = '';
+}
+
+function clickMinus() {
+    input1 = parseFloat(document.forms.result.value);
+    // save value to variable and operator
+    operator = '-';
+    document.forms.result.value = '';
+}
+
+function clickMulti() {
+    input1 = parseFloat(document.forms.result.value);
+    // save value to variable and operator
+    operator = '*';
+    document.forms.result.value = '';
+}
+
+function clickDivid() {
+    input1 = parseFloat(document.forms.result.value);
+    // save value to variable and operator
+    operator = '/';
+    document.forms.result.value = '';
+}
+
+function clickEqual() {
+    input2 = parseFloat(document.forms.result.value);
+    // 더하기 수행 (operator가 +일때)
+    if (operator === '+') {
+       answer = input1 + input2;
+    } else if (operator === '-') {
+        answer = input1 - input2;
+    } else if (operator === '*') {
+        answer = input1 * input2;
+    } else if (operator === '/') {
+        answer = input1 / input2;
+    } else {
+        document.write = "error!"
+    }
+
+    // 결과 표시
+    document.forms.result.value = answer;
 }
